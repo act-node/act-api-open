b890f66ee515c64a994681c7dd932f9ec812393f
Masaki Momozu <59199162+act-node@users.noreply.github.com>
2021-12-18 18:06:20 +0900

    data update

---
+++ request_zeta.ts
@@ -0,0 +1,55 @@
+const sample = {
+    "type": "metric",
+    "version": "v1",
+    "payload": {
+        "device": "111513318ABA0006",
+        "timestamp": 1639817016,
+        "data": [
+            {
+                "port": "",
+                "name": "battery",
+                "value": 98
+            },
+            {
+                "port": "",
+                "name": "battery_status",
+                "value": 2
+            },
+            {
+                "port": "A1-1",
+                "name": "water_oxygen",
+                "value": 15.6
+            },
+            {
+                "port": "A1-1",
+                "name": "water_temp",
+                "value": 20.900000000000002
+            },
+            {
+                "port": "A1-3",
+                "name": "water_ph",
+                "value": 12.22
+            },
+            {
+                "port": "A1-4",
+                "name": "air_temp",
+                "value": 7.6
+            },
+            {
+                "port": "A1-4",
+                "name": "air_hum",
+                "value": 95
+            },
+            {
+                "port": "A1-4",
+                "name": "illuminance",
+                "value": 1
+            },
+            {
+                "port": "A1-5",
+                "name": "water_conductivity",
+                "value": 0.007
+            }
+        ]
+    }
+}
